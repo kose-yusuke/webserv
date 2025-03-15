20e2ca8ab37c646f5edc11c897629bca2559c9db
skitheom <SahoKitahara@my.uopeople.edu>
2025-03-15 16:24:10 +0900

    refactor: config系の型名にtypedefを追加

---
+++ includes/types.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+
+// 汎用
+typedef std::vector<std::string> StrVector;
+typedef std::map<std::string, std::string> StrToStrMap;
+
+// config
+typedef std::map<std::string, std::vector<std::string> > ConfigMap;
+typedef std::map<std::string, std::map<std::string, std::vector<std::string > > > LocationMap;
+typedef std::vector<std::pair<ConfigMap, LocationMap> > ServerAndLocationConfigs;
